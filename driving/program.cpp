7b27bdb4fabed8ddb76dc4082930f1b299b79930
Jakob Bo Søndergaard Madsen <jakobbsm@gmail.com>
2017-09-17 13:12:54 +0200

    Converted indentation to tabs

--- driving/program.cpp
+++ driving/program.cpp
@@ -37,78 +37,89 @@ void user_1ms_isr_type2(void)
 	SleeperMonitor(); // needed for I2C device and Clock classes
 }
 
+/**
+ * Which direction to turn towards.
+ *
+ * @param direction
+ */
 void turn(int direction)
 {
-    if (direction == TURN_LEFT)
-        motorTurn.setPWM(TURN_SPEED);
+	if (direction == TURN_LEFT)
+		motorTurn.setPWM(TURN_SPEED);
 
-    else if (direction == TURN_RIGHT)
-        motorTurn.setPWM(-(TURN_SPEED));
+	else if (direction == TURN_RIGHT)
+		motorTurn.setPWM(-(TURN_SPEED));
 
-    else if (direction == TURN_CENTER)
-    {
-        int angle = motorTurn.getCount();
+	else if (direction == TURN_CENTER) // TODO: Need to think through the approach on this one
+	{
+		int angle = motorTurn.getCount();
 
-        if (angle > 3 && angle < -3)
-            if (angle > 3)
-                motorTurn.setPWM(-(TURN_SPEED));
-            else if (angle < -3)
-                motorTurn.setPWM(TURN_SPEED);
-        else
-            motorTurn.setPWM(0);
-    }
+		if (angle > 3 && angle < -3)
+			if (angle > 3)
+				motorTurn.setPWM(-(TURN_SPEED));
+			else if (angle < -3)
+				motorTurn.setPWM(TURN_SPEED);
+		else
+			motorTurn.setPWM(0);
+	}
 }
 
+/**
+ * Select between drive / stop / reverse
+ *
+ * @param direction
+ */
 void drive(int direction)
 {
-    if (direction == DRIVE_FORWARD)
-        motorForward.setPWM(FORWARD_SPEED);
-    else if (direction == DRIVE_REVERSE)
-        motorForward.setPWM(-(FORWARD_SPEED));
-    else
-        motorForward.setPWM(0);
-    motorForward.setCount(0);
+	if (direction == DRIVE_FORWARD)
+		motorForward.setPWM(FORWARD_SPEED);
+	else if (direction == DRIVE_REVERSE)
+		motorForward.setPWM(-(FORWARD_SPEED));
+	else
+		motorForward.setPWM(0);
+
+	motorForward.setCount(0); // Counter is irrelevant and an increasing number would be a waste.
 }
 
 TASK(TaskMain)
 {
-    Nxt nxt; // Used for buttons
+	Nxt nxt; // Used for buttons
 	Lcd lcd;
 	Clock clock;
-    int direction = -1;
-    int distance;
+	int direction = -1;
+	int distance;
 
-    drive(DRIVE_FORWARD);
+	drive(DRIVE_FORWARD);
 
 	while(1)
 	{
-        distance = sonar.getDistance();
+		distance = sonar.getDistance();
 
-        if (distance < 10)
-        {
-            direction = 0;
-            // Reverse
-            drive(DRIVE_REVERSE);
-            turn(TURN_RIGHT);
-            clock.wait(2000);
-            drive(DRIVE_FORWARD);
-            direction = TURN_LEFT;
-        }
-        else if (direction == DRIVE_STOP)
-            direction = TURN_LEFT;
+		if (distance < 10)
+		{
+			direction = 0;
+			// Reverse
+			drive(DRIVE_REVERSE);
+			turn(TURN_RIGHT);
+			clock.wait(2000);
+			drive(DRIVE_FORWARD);
+			direction = TURN_LEFT;
+		}
+		else if (direction == DRIVE_STOP)
+			direction = TURN_LEFT;
 
-        // Left right spasm
-        if (direction == TURN_LEFT && motorTurn.getCount() > TURN_ANGLE)
-            direction = TURN_RIGHT;
-        else if (direction == TURN_RIGHT && motorTurn.getCount() < -(TURN_ANGLE))
-            direction = TURN_LEFT;
+		// Left right spasm
+		if (direction == TURN_LEFT && motorTurn.getCount() > TURN_ANGLE)
+			direction = TURN_RIGHT;
+		else if (direction == TURN_RIGHT && motorTurn.getCount() < -(TURN_ANGLE))
+			direction = TURN_LEFT;
 
-        turn(direction);
+		turn(direction);
 
-        lcd.clear();
-        lcd.putf("sdn", "Distance: ", distance, 5);
-        lcd.putf("sdn", "TurnA: ", motorTurn.getCount(), 0);
-        lcd.putf("sdn", "Direction: ", direction);
+		lcd.clear();
+		lcd.putf("sdn", "Distance: ", distance, 5);
+		lcd.putf("sdn", "TurnA: ", motorTurn.getCount(), 0);
+		lcd.putf("sdn", "Direction: ", direction);
 		lcd.disp();
 
 		clock.wait(100);
